22be146e6881580de48fc4a070d1921d3d27c2d6
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2023-12-18 20:03:45 +0100

    create config

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+function dd($var)
+{
+    echo '<code>';
+    echo '<pre>';
+    print_r($var);
+    echo '</pre>';
+    echo '</code>';
+    die();
+}
+
+define('__URI__', "http://" . $_SERVER['HTTP_HOST'] . "/MNAOUERFatimaezzahra_ticket_manager/");
+
+$db = mysqli_connect('localhost', 'root', '', 'ticket_db');
