7f42ac2358c6f6f06ef5436e3c7831c342321b35
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2023-12-20 12:36:42 +0100

    Update ticket_form_controller.php

--- controllers/ticket_form_controller.php
+++ controllers/ticket_form_controller.php
@@ -1,15 +1,17 @@
 <?php
 
-if (isset($_POST['tickets_title']) && !empty($_POST['tickets_title']) && isset($_POST['tickets_desc']) && !empty($_POST['tickets_desc']) && isset($_POST['tickets_status']) && !empty($_POST['tickets_status']) && isset($_POST['tickets_priority']) && !empty($_POST['tickets_priority'])) {
-    // Instantier la classe utilisateur
-    include_once 'models/Ticket.php';
-    $ticket = new Ticket();
+dd($_POST);
 
-    $result = $ticket->create($_POST['tickets_title'], $_POST['tickets_desc'], $_POST['tickets_status'], $_POST['tickets_priority']);
-
-    if ($result) {
-        header("Location:" . __URI__ . "index.php?page=home");
-    } else {
-        header("Location:" . __URI__ . "index.php?page=ticket_form");
-    }
-}
+//if (isset($_POST['tickets_title']) && !empty($_POST['tickets_title']) && isset($_POST['tickets_desc']) && !empty($_POST['tickets_desc']) && isset($_POST['tickets_status']) && !empty($_POST['tickets_status']) && isset($_POST['tickets_priority']) && !empty($_POST['tickets_priority'])) {
+//    // Instantier la classe utilisateur
+//    include_once 'models/Ticket.php';
+//    $ticket = new Ticket();
+//
+//    $result = $ticket->create($_POST['tickets_title'], $_POST['tickets_desc'], $_POST['tickets_status'], $_POST['tickets_priority']);
+//
+//    if ($result) {
+//        header("Location:" . __URI__ . "index.php?page=home");
+//    } else {
+//        header("Location:" . __URI__ . "index.php?page=ticket_form");
+//    }
+//}
